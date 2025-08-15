840e631bacc920957bb3ad92564449c8b0ecc465
jpadie <jpadie@me.com>
2025-08-15 09:35:38 +0100

    Fix Node-RED palette issue: include HTML files in npm package

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { createWaveshareDANode } from './nodes/waveshare-da';
 import { createWaveshareADNode } from './nodes/waveshare-ad';
 
 module.exports = function(RED: any) {
+  // Register the DAC node
   RED.nodes.registerType('waveshare-da', createWaveshareDANode(RED));
+  
+  // Register the ADC node
   RED.nodes.registerType('waveshare-ad', createWaveshareADNode(RED));
 };
 
